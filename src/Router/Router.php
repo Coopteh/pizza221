3874f852f900b59aae10c96d01ca90f4fc596784
Студент 8 Кабинет 409 <409-8@coopteh.local>
2025-03-11 11:36:38 +0700

    Главная страница с каруселью и описанием

---
+++ src/Router/Router.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace App\Router;
+use App\Controllers\HomeController;
+use App\Controllers\AboutController;
+
+class Router {
+    public function route(string $url): ?string {
+        $path = parse_url($url, PHP_URL_PATH);
+        $pieces = explode("/", $path);
+        $resource = $pieces[2];
+        switch ($resource) {
+            case "about":
+                $about = new AboutController();
+                return $about->get();
+            default:
+                $home = new HomeController();
+                return $home->get();
+        }
+    }
+}
+
+
--- src/Views/HomeTemplate.php
+++ src/Views/HomeTemplate.php
@@ -1,29 +1,48 @@
-<?php
+<?php 
 namespace App\Views;
+
+use App\Views\BaseTemplate;
+
 class HomeTemplate extends BaseTemplate {
-    
-    public static function getTemplate():string {
+    public static function getTemplate(): string {
         $template = parent::getTemplate();
         $title= 'Главная страница';
-        $content = <<<LINE
-<section>  
-<div class="h-50 w-50mx-auto">             
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carusel">
-  <div class="carousel-inner" style="height:50vh">
-    <div class="carousel-item active">
-      <img src="./assets/images/img1.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./assets/images/img2.png" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./assets/images/img3.png" class="d-block w-100" alt="...">
-    </div>
-  </div>
-</section>  
-LINE;
-
-        $template =  sprintf($template, $title, $content);
-        return $template;
+        $content = <<<CORUSEL
+        <section>        
+            <div class="h-50 w-50 mx-auto">        
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner" style="height:65vh;">
+                        <div class="carousel-item active">
+                        <img src="./assets/images/img1.png" class="d-block w-100 h-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="./assets/images/img2.png" class="d-block w-100 h-100 " alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="./assets/images/img3.png" class="d-block w-100 h-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>
+            </div>
+        </section>
+        <main class="row">
+            <div class="p-5">
+                <p>Здесь можно заказать шаурму и шашлык с доставкой по городу Кемерово.</p>
+                <p>Широкий ассортимент, низкие цены, быстрая доставка!<br><br></p>
+                <p> (*) Сайт разработан в рамках обучения в "Кузбасском кооперативном техникуме" по специальности "Специалист по информационным технологиям".</p>
+            </div>
+        </main>        
+        CORUSEL;
+        
+        $resultTemplate =  sprintf($template, $title, $content);
+        return $resultTemplate;
     }
 }
