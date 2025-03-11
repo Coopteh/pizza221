639f0148938c9abb515a216aeb79294b68d184d7
TotalBezdar <409-6@coopteh.local>
2025-03-11 11:17:25 +0700

    добавлен роутер

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 require_once("./vendor/autoload.php");
 
-use App\Controllers\HomeController;
+use App\Router\Router;
 
-$controller = new HomeController();
-echo $controller->get();
+$router = new Router();
+$url = $_SERVER['REQUEST_URI'];
+echo $router->route($url);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'kkt/pizza221',
         'pretty_version' => 'dev-409/06/bfg',
         'version' => 'dev-409/06/bfg',
-        'reference' => 'c6632a1bdc4254f989c6a8b5f6849de17f3e2fd1',
+        'reference' => '70f8d56caa2f0f708db8468b50465b400fd3a5e8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'kkt/pizza221' => array(
             'pretty_version' => 'dev-409/06/bfg',
             'version' => 'dev-409/06/bfg',
-            'reference' => 'c6632a1bdc4254f989c6a8b5f6849de17f3e2fd1',
+            'reference' => '70f8d56caa2f0f708db8468b50465b400fd3a5e8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
