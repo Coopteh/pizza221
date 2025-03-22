91163d7680b23ea0e104507954c5432486dcc3cc
Студент 15 Кабинет 409 <409-15@coopteh.local>
2025-03-22 16:57:27 +0700

    Добавлена информация о доставке

--- src/Views/HomeTemplate.php
+++ src/Views/HomeTemplate.php
@@ -14,7 +14,7 @@ class HomeTemplate extends BaseTemplate
         <link href="assets/css/style.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
-    
+    		<p> "Бесплатная доставка при заказе от 500 рублей"   </p>
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
             <div class="carousel-inner" style="max-width: 540px;">
                 <div class="carousel-item active">
