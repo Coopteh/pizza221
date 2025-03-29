12078016e05706bd1132bea78425fd8ec9494383
Студент 7 Кабинет 409 <409-7@coopteh.local>
2025-03-29 17:02:14 +0700

    fk

--- src/Views/OrderTemplate.php
+++ src/Views/OrderTemplate.php
@@ -66,6 +66,10 @@ class OrderTemplate extends BaseTemplate{
                     <label for="phone" class="form-label">Телефон:</label>
                     <input type="tel" class="form-control" id="phone" name="phone" required>
                 </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Mail:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
                 <button type="submit" class="btn btn-primary">Создать заказ</button>
             </form>
             HTML;
