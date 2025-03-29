ee566da8b5e8e83e110547cd9a5b0a40bd063efb
Студент 11 Кабинет 409 <409-11@coopteh.local>
2025-03-29 17:29:18 +0700

    валидация

--- src/Views/OrderTemplate.php
+++ src/Views/OrderTemplate.php
@@ -1,103 +1,90 @@
-<?php 
+<?php
+
 namespace App\Views;
 
 use App\Views\BaseTemplate;
 
-class OrderTemplate extends BaseTemplate
-{
-    public static function getOrderTemplate(array $products): string {
+class OrderTemplate extends BaseTemplate {
+    public static function getOrderTemplate(array $arr): string {
         $template = parent::getTemplate();
-        $title= 'Создание заказа';
-        $content = <<<CORUSEL
-        <main class="row p-5">
-            <h1 class="mb-5">Создание заказа</h1>
-            <h3>Список товаров (Корзина)</h1>
-        CORUSEL;
-
+        $title = 'Создать заказ';
+        $content = '<h1 class="mb-5">Создание заказа</h1><h3>Корзина</h3>';
         $all_sum = 0;
-        foreach ($products as $product) {
-		    $name = $product['name'];
-            $price = $product['price'];
-		    $quantity = $product['quantity'];
 
-            $sum = $price * $quantity;
-            $all_sum += $sum;
-
-            $content .= <<<LINE
-                <div class="row">
-                    <div class="col-5">
-                    {$name}
-                    </div>
-                    <div class="col-3">
-                    {$quantity} ед. x {$price} руб.
-                    </div>
-                    <div class="col-2">
-                    {$sum} ₽
-                    </div>
-                </div>
-            LINE;
-	    }
-
-        if ($all_sum == 0) {
-            $content .= <<<LINE
+        if (empty($arr)) {
+            $content .= <<<HTML
             <div class="row">
                 <div class="col-12">
-                - нет добавленных товаров -
+                    - нет добавленных товаров -
                 </div>
             </div>
-            LINE;
+            HTML;
         } else {
-            $content .= <<<LINE
+            foreach ($arr as $product) {
+                $name = htmlspecialchars($product['name'], ENT_QUOTES, 'UTF-8');
+                $price = htmlspecialchars($product['price'], ENT_QUOTES, 'UTF-8');
+                $quantity = htmlspecialchars($product['quantity'], ENT_QUOTES, 'UTF-8');
+                $sum = htmlspecialchars($product['sum'], ENT_QUOTES, 'UTF-8');
+                $all_sum += $sum;
+
+                $content .= <<<HTML
                 <div class="row">
-                    <hr>
-                    <div class="col-5">
-                        <strong>Общая сумма:</strong>
-                    </div>
-                    <div class="col-3">
-                        &nbsp;
-                    </div>
-                    <div class="col-2">
-                        <strong>{$all_sum} ₽</strong>
-                    </div>
-                </div>    
-
-                <div class="row">
-                    <div class="col-8">
-                        &nbsp;
-                    </div>
-                    <div class="col-2 float-end">
-                        <form action="/pizza221/basket_clear" method="POST">
-                            <button type="submit" class="btn btn-secondary mt-3">Очистить корзину
-                        </form>
-                    </div>
-                </div>    
-
-            LINE;
-
-            $content .= <<<FORMA
-                <h3>Данные для доставки</h1>
-                <form action="/pizza221/order" method="POST">
-                    <div class="mb-3">
-                        <label for="fioInput" class="form-label">Ваше имя (ФИО):</label>
-                        <input type="text" name="fio" class="form-control" id="fioInput" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="addressInput" class="form-label">Адрес доставки:</label>
-                        <input type="text" name="address" class="form-control" id="addressInput">
-                    </div>
-                    <div class="mb-3">
-                        <label for="phoneInput" class="form-label">Телефон:</label>
-                        <input type="text" name="phone" class="form-control" id="phoneInput">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Создать заказ</button>
-                </form>
-            FORMA;
-
+                    <div class="col-6">{$name}</div>
+                    <div class="col-2">{$quantity} ед. x {$price} руб.</div>
+                    <div class="col-2">{$sum} ₽</div>
+                </div>
+                HTML;
+            }
         }
 
-        $content .= "</main>";
+        // Итоговая сумма
+        if ($all_sum > 0) {
+            $content .= <<<HTML
+            <div class="row">
+                <div class="col-6">Итого:</div>
+                <div class="col-2">{$all_sum} ₽</div>
+            </div>
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-6 float-end">
+                    <form action="/trenazherka/basket_clear" method="POST">
+                        <button type="submit" class="btn btn-secondary mt-3">Очистить корзину</button>
+                    </form>
+                </div>
+            </div>
+            HTML;
+        }
 
-        $resultTemplate =  sprintf($template, $title, $content);
+        // Добавляем HTML-код формы
+        $content .= self::getOrderForm();
+
+        // Возвращаем сгенерированный контент
+        $resultTemplate = sprintf($template, $title, $content);
         return $resultTemplate;
+    }
+
+    private static function getOrderForm(): string {
+        // HTML-код формы
+        return '
+            <form action="/pizza221/order" method="POST">
+                <div class="mb-3">
+                    <label for="fio" class="form-label">Ваше ФИО:</label>
+                    <input type="text" name="fio" id="fio" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Адрес доставки:</label>
+                    <input type="text" name="address" id="address" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label for="phone" class="form-label">Телефон:</label>
+                    <input type="tel" name="phone" id="phone" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail:</label>
+                    <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Создать заказ</button>
+            </form>
+        ';
     }
 }
