25d8352164b3163b32bea66764d6b070a150a8bf
Студент 10 Кабинет 409 <409-10@coopteh.local>
2025-03-31 15:49:26 +0700

    Информацияы о заказе

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,22 +1,28 @@
 <?php
-
 namespace App\Controllers;
 
 use App\Models\Product;
 use App\Views\ProductTemplate;
+use App\Services\FileStorage;
+use App\Services\DatabaseStorage;
+use App\Configs\Config;
 
-class ProductController
-{
-    public function get(?int $id = null): string {
-        $model = new Product();
-        $data = $model->loadData();
-        
-        if (isset($data[$id-1])) {
-            $data = $data[$id-1];
-        } else {
-            return ProductTemplate::getAllTemplate($data);
+class ProductController {
+    public function get(?int $id): string {
+
+        if (Config::STORAGE_TYPE == Config::TYPE_FILE) {
+            $serviceStorage = new FileStorage();
+            $model = new Product($serviceStorage, Config::FILE_PRODUCTS);
         }
-        
-        return ProductTemplate::getCardTemplate($data);
+
+        $data = $model->loadData();
+
+        if (!isset($id))
+            return ProductTemplate::getAllTemplate($data);
+        if (($id) && ($id <= count($data))) {
+            $record= $data[$id-1];
+            return ProductTemplate::getCardTemplate($record);
+        } else
+            return ProductTemplate::getCardTemplate(null);
     }
 }
