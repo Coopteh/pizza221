47d335e2efa4cec8922f4712b72ee1e07e63dfde
Agility800 <409-4@coopteh.local>
2025-03-31 13:01:49 +0700

    Сохранение заказа

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -2,6 +2,8 @@
 namespace App\Controllers;
 use App\Models\Product;
 use App\Views\OrderTemplate;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 class OrderController{
     public function get(): string{
         // метод GET, POST, DELETE
@@ -83,5 +85,40 @@ class OrderController{
         }
     
         return true;
+    }
+    public function sendMail($email) {
+        $mail = new PHPMailer();
+        if (isset($email) && !empty($email)) {
+            try {
+                $mail->SMTPDebug = 2;
+                $mail->CharSet = 'UTF-8';
+                $mail->SetFrom("v.milevskiy@coopteh.ru","PIZZA-221");
+                $mail->addAddress($email);
+                $mail->isHTML(true);
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'ssl://smtp.mail.ru';                   //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'v.milevskiy@coopteh.ru';                     //SMTP username
+                $mail->Password   = 'qRbdMaYL6mfuiqcGX38z';
+                $mail->Port       = 465;
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Subject = 'Заявка с сайта: PIZZA-221';
+                $mail->Body = "Информационное сообщение c сайта PIZZA-221 <br><br>
+                ------------------------------------------<br><br>
+                Спасибо!<br><br>
+                Ваш заказ успешно создан и передан службе доставки.<br><br>
+                Сообщение сгенерировано автоматически.";
+                // отправка емайл
+                $this->sendMail($email);
+                if ($mail->send()) {
+                    return true;
+                } else {
+                    throw new Exception('Ошибка с отправкой письма');
+                }
+            } catch (Exception $error) {
+                $message = $error->getMessage();
+            }
+        }
+        return false;
     }
 }
