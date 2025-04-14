803690e3c5a52bb73238d06832c9f4975ffb6d6c
Студент 14 Кабинет 409 <409-14@coopteh.local>
2025-04-14 11:58:43 +0300

    Тест валидации даных

--- tests/OrderDataTest.php
+++ tests/OrderDataTest.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Tests;
-
 use PHPUnit\Framework\TestCase;
 use App\Services\ValidateOrderData;
 
@@ -11,7 +10,6 @@ class OrderDataTest extends TestCase
     private ValidateOrderData $obj;
 
     public function setUp():void {
-        // Массив валидных данных для передачи в метод
         $this->data = [];
         $this->data['fio'] = "Иванов";
         $this->data['address'] = "Кемерово, ул.Тухачевского 32";
@@ -24,5 +22,42 @@ class OrderDataTest extends TestCase
     public function testValidateOrderData(): void {
         $this->assertSame( true, 
                            $this->obj->validate($this->data) );
+    }
+
+    // ФИО - заполнено
+    public function testInvalidFio(): void {
+        $invalidFio = [
+            'fio' => [],
+        ];
+        $this->assertSame(false, 
+                          $this->obj->validate($invalidFio));
+    }
+
+    // адрес > 10
+    public function testInvalidAddress(): void {
+        $invalidAddress = [
+            'address' => ''
+        ];
+        $this->assertSame(false, 
+                          $this->obj->validate($invalidAddress));
+    }
+
+
+    // телефон - 11 цифр, 7 либо 8 в начале
+    public function testInvalidPhone(): void {
+        $this->data['phone'] = "1234567890"; 
+        $this->assertFalse($this->obj->validate($this->data));
+        $this->data['phone'] = "19007009911";
+        $this->assertFalse($this->obj->validate($this->data));
+    }
+
+    // емайл - невалидные адреса проверить, типа "invalid", "@missing.username", ""
+    public function testInvalidEmail(): void {
+        $this->data['email'] = "invalid";
+        $this->assertFalse($this->obj->validate($this->data)); 
+        $this->data['email'] = "@missing.username"; 
+        $this->assertFalse($this->obj->validate($this->data));
+        $this->data['email'] = "";
+        $this->assertFalse($this->obj->validate($this->data));
     }
 }
