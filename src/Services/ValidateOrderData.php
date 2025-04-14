3b38628a6b79a50674b757be6842ede1a3543453
Wolfthena <409-2@coopteh.local>
2025-04-14 15:34:34 +0700

    добовление тестов

--- src/Services/ValidateOrderData.php
+++ src/Services/ValidateOrderData.php
@@ -2,40 +2,32 @@
 namespace App\Services;
 
 class ValidateOrderData {
-    public static function validate(array $data): bool {
+    public function validate(array $data): bool {
         // Проверка ФИО
-        if (!isset($data['fio'])) {
-            $_SESSION['flash'] = "Незаполнено поле ФИО.";
+        if (empty($data['fio'])) {
             return false;
         }
-    
+
         // Проверка адреса
-        if (!isset($data['address']) || 
-            strlen(trim($data['address'])) < 10 || 
-            strlen(trim($data['address'])) > 200) {
-            $_SESSION['flash'] = "Поле адреса должно быть более 10 символов (но не более 200).";
+        if (strlen($data['address']) < 10) {
             return false;
         }
-    
+
         // Проверка телефона
-        if (!isset($data['phone'])) {
-            $_SESSION['flash'] = "Незаполнено поле Телефон.";
+        if (!$this->isValidPhone($data['phone'])) {
             return false;
         }
-        $cleanedPhone = preg_replace('/[^\\d]/', '', $data['phone']);
-        if (strlen($cleanedPhone) !== 11 || 
-            !in_array($cleanedPhone[0], ['7', '8'])) {
-            $_SESSION['flash'] = "Неверный номер телефона.";
-            return false;
-        }
-    
+
         // Проверка email
-        if (!isset($data['email']) || 
-            !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-            $_SESSION['flash'] = "Неправильно заполнено поле Емайл.";
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
             return false;
         }
-    
+
         return true;
+    }
+
+    private function isValidPhone(string $phone): bool {
+        // Пример простой проверки номера телефона
+        return preg_match('/^8[0-9]{10}$/', $phone);
     }
 }
