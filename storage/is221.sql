481871fa27fd97aede3074e7f94684eb67b41907
Wolfthena <409-2@coopteh.local>
2025-04-15 11:25:57 +0700

    Создание сиситемы регистрации

--- storage/is221.sql
+++ storage/is221.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 08 2025 г., 04:23
+-- Время создания: Апр 15 2025 г., 06:23
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -37,6 +37,18 @@ CREATE TABLE `orders` (
   `created` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `fio`, `address`, `phone`, `email`, `all_sum`, `created`) VALUES
+(1, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'example@mail.ru', 6050, '2025-04-08 12:28:06'),
+(2, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'hitrucetru@gufum.com', 6050, '2025-04-08 12:28:43'),
+(3, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'hitrucetru@gufum.com', 0, '2025-04-08 12:30:22'),
+(4, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'hitrucetru@gufum.com', 3300, '2025-04-08 12:34:09'),
+(5, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'example@mail.ru', 4400, '2025-04-15 10:54:54'),
+(6, 'Иванов Иван Иванович', 'г. Москва, ул. Ленина, д. 15, кв. 42', '79161234567', 'hitrucetru@gufum.com', 4400, '2025-04-15 10:55:08');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +63,17 @@ CREATE TABLE `order_item` (
   `price_item` float NOT NULL,
   `sum_item` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Дамп данных таблицы `order_item`
+--
+
+INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `count_item`, `price_item`, `sum_item`) VALUES
+(1, 1, 1, 11, 550, 6050),
+(2, 2, 1, 11, 550, 6050),
+(3, 4, 1, 6, 550, 3300),
+(4, 5, 1, 8, 550, 4400),
+(5, 6, 1, 8, 550, 4400);
 
 -- --------------------------------------------------------
 
@@ -75,6 +98,29 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `created`, `updated`) VALUES
 (1, 'Пицца Маргарита', 'Это наша фирменная пицца, она состоит из куриного мяса, сосисок, грибов и 2 сортов сыра (пармезана и моцареллы), декорирована веточкой петрушки!', 'https://localhost/pizza221/assets/images/image1.png', 550, '2025-04-08 09:20:30', '2025-04-08 09:20:30'),
 (2, 'Пицца Пипперони', 'Это знаменитая пицца с колбасой и сыром', 'https://localhost/pizza221/assets/images/image2.png', 650, '2025-04-08 09:20:30', '2025-04-08 09:20:30');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `token`, `is_verified`, `created_at`) VALUES
+(1, 'Дамболдор', 'example@mail.ru', '$2y$10$EtMb4O3E5evxt.2H01Ou0uCu93M//W81CuSU6HExAYjnU/O.jMEOe', '459abdbd9cecdfc1cf2a4f93d27ea529', 0, '2025-04-15 11:22:12');
 
 --
 -- Индексы сохранённых таблиц
@@ -102,6 +148,13 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -109,19 +162,25 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
--- tests/OrderDataTest.php
+++ tests/OrderDataTest.php
@@ -1,4 +1,4 @@
-<?php  
+<?php 
 
 use PHPUnit\Framework\TestCase;
 use App\Services\ValidateOrderData;
@@ -8,88 +8,53 @@ class OrderDataTest extends TestCase
     private array $data;
     private ValidateOrderData $obj;
 
-    public function setUp(): void {
+    public function setUp():void {
         // Массив валидных данных для передачи в метод
         $this->data = [];
         $this->data['fio'] = "Иванов";
         $this->data['address'] = "Кемерово, ул.Тухачевского 32";
         $this->data['phone'] = "89007009911";
         $this->data['email'] = "ivanov@example.com";
-        
         // Объект класса ValidateOrderData
         $this->obj = new ValidateOrderData();
     }
 
     public function testValidateOrderData(): void {
-        $this->assertSame(true, 
-                          $this->obj->validate($this->data));
+        $this->assertSame( true, 
+                           $this->obj->validate($this->data) );
     }
 
-    // Набор тестов для невалидных данных
-    public function testInvalidFio(): void {
-        $invalidFio = [
-            'fio' => '', // пустое ФИО
-            'address' => 'Кемерово, ул.Тухачевского 32',
-            'phone' => '89007009911',
-            'email' => 'ivanov@example.com'
-        ];
-        $this->assertSame(false, 
-                          $this->obj->validate($invalidFio));
+    // ФИО - заполнено
+    public function testFioNotValidate(): void {
+        unset($this->data['fio']);
+        $this->assertSame( false, 
+                           $this->obj->validate($this->data) );
     }
-
-    public function testInvalidAddress(): void {
-        $invalidAddress = [
-            'fio' => 'Иванов',
-            'address' => 'К', // адрес меньше 10 символов
-            'phone' => '89007009911',
-            'email' => 'ivanov@example.com'
-        ];
-        $this->assertSame(false, 
-                          $this->obj->validate($invalidAddress));
+    // адрес > 10
+    public function testAddressNotValidate(): void {
+        $this->data['address'] = "Мало";
+        $this->assertSame( false, 
+                           $this->obj->validate($this->data) );
     }
-
-    public function testInvalidPhone(): void {
-        // Тесты на невалидные телефоны
-        $invalidPhones = [
-            ['fio' => 'Иванов', 'address' => 'Кемерово, ул.Тухачевского 32', 'phone' => '1234567', 'email' => 'ivanov@example.com'], // слишком короткий номер
-            ['fio' => 'Иванов', 'address' => 'Кемерово, ул.Тухачевского 32', 'phone' => '8900700991a', 'email' => 'ivanov@example.com'], // буквы в номере
-            ['fio' => 'Иванов', 'address' => 'Кемерово, ул.Тухачевского 32', 'phone' => '', 'email' => 'ivanov@example.com'], // пустой номер
-            ['fio' => 'Иванов', 'address' => 'Кемерово, ул.Тухачевского 32', 'phone' => '+79007009911', 'email' => 'ivanov@example.com'], // неверный формат (начинается с +)
-        ];
-
-        foreach ($invalidPhones as $invalidPhone) {
-            $this->assertSame(false, 
-                              $this->obj->validate($invalidPhone));
-        }
+    // телефон - 11 цифр, 7 либо 8 в начале
+    public function testPhoneNotValidate(): void {
+        $this->data['phone'] = "44-55-66";
+        $this->assertSame( false, 
+                           $this->obj->validate($this->data) );
+        $this->data['phone'] = "19004556677";
+        $this->assertSame( false, 
+                            $this->obj->validate($this->data) );                           
     }
-
-    public function testInvalidEmail(): void {
-        // Тесты на невалидные email адреса
-        $invalidEmails = [
-            ['fio' => 'Иванов', 
-             'address' => "Кемерово, ул.Тухачевского 32", 
-             'phone' => "89007009911", 
-             'email' => "invalid"], // некорректный email
-            
-            ['fio' => "Иванов", 
-             "address" => "Кемерово, ул.Тухачевского 32", 
-             "phone" => "89007009911", 
-             "email" => "@missing.username"], // отсутствует имя пользователя
-            
-            ['fio' => "Иванов", 
-             "address" => "Кемерово, ул.Тухачевского 32", 
-             "phone" => "89007009911", 
-             "email" => ""], // пустой email
-            
-            ['fio' => "Иванов", 
-             "address" => "Кемерово, ул.Тухачевского 32", 
-             "phone" => "89007009911", 
-             "email" => "ivanov@.com"], // некорректный домен
-        ];
-
-        foreach ($invalidEmails as $invalidEmail) {
-            $this->assertSame(false, 
-                              $this->obj->validate($invalidEmail));
-        }
+    // емайл - невалидные адреса проверить, типа "invalid", "@missing.username", ""
+    public function testEmailNotValidate(): void {
+        $this->data['email'] = "invalid";
+        $this->assertSame( false,
+                           $this->obj->validate($this->data) );
+        $this->data['phone'] = "@missing.username";
+        $this->assertSame( false,
+                            $this->obj->validate($this->data) );
+        $this->data['phone'] = "";
+        $this->assertSame( false,
+                            $this->obj->validate($this->data) );
     }
 }
