fedf497a4df1dd8aaf15a2e9b933044d38dd0eb1
Студент 11 Кабинет 409 <409-11@coopteh.local>
2025-04-22 15:27:15 +0700

    Вход в пользователя

--- src/Controllers/BasketController.php
+++ src/Controllers/BasketController.php
@@ -2,37 +2,35 @@
 
 namespace App\Controllers;
 
-use App\Views\AboutTemplate;
+class BasketController
+{
+    public function add(): void
+    {
 
-class BasketController {
-    public function add():void {
-        session_start();
-        
         if (isset($_POST['id'])) {
             $product_id = $_POST['id'];
-        
             if (!isset($_SESSION['basket'])) {
                 $_SESSION['basket'] = [];
             }
-        
+
             if (isset($_SESSION['basket'][$product_id])) {
                 $_SESSION['basket'][$product_id]['quantity']++;
             } else {
                 $_SESSION['basket'][$product_id] = [
-                    'quantity' => 1
+                'quantity' => 1
                 ];
             }
-        //var_dump($_SESSION);
-            $_SESSION['flash'] = "Товар успешно добавлен в корзину!"; 
-        //exit();
+            //var_dump($_SESSION);
+            //exit();
+            $_SESSION['flash'] = "Товар успешно добавлен в корзину!";
         }
     }
-    /* 
+    /*
     Очистка корзины
     */
-    public function clear():void {
-        session_start();
-        $_SESSION['basket'] = []; // Очищаем корзину
-        $_SESSION['flash'] = "Корзина успешно очищена."; // Уведомление пользователю
+    public function clear(): void
+    {
+        $_SESSION['basket'] = [];
+        $_SESSION['flash'] = "Корзина успешно очищена.";
     }
 }
--- src/Views/BaseTemplate.php
+++ src/Views/BaseTemplate.php
@@ -3,7 +3,8 @@ namespace App\Views;
 class BaseTemplate 
 {
     public static function getTemplate(): string {
-        $template = '';
+        global $user_id, $username;
+
         $template = <<<HTML
         <!DOCTYPE html>
         <html lang="ru">
@@ -19,8 +20,8 @@ class BaseTemplate
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
-                        <img src="/avtoservis/assets/images/logo.png" alt="Логотип компании" width="64" height="64">
-                        Автосервис ИС-221
+                        <img src="https://localhost/avtoservis/assets/images/logo.png" alt="Логотип компании" width="64" height="64">
+                        АВТОСЕРВИС
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -33,40 +34,61 @@ class BaseTemplate
                         <li class="nav-item">
                         <a class="nav-link" href="/avtoservis/products">Каталог</a>
                         </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/avtoservis/about">О нас</a>
+                        </li>
+                        <li class="nav-item">
                         <a class="nav-link" href="/avtoservis/order">Заказ</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="/avtoservis/about">О нас</a>
-                        <li class="nav-item">
-                        <a class="nav-link" href="/avtoservis/register">Регистарция</a>
+                        <a class="nav-link" href="/avtoservis/register">Регистрация</a>
                         </li>
                     </ul>
                     </div>
                 </div>
-                </nav>
-            </header>
-HTML;
-// Добавим flash сообщение
-if(!isset($_SESSION))
-{
-    session_start();
+        HTML;
+
+if ($user_id > 0) {
+        $template .= <<<HTML
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {$username}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/avtoservis/profile">Профиль</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="/avtoservis/logout">Выход</a></li>
+                        </ul>
+                    </li>
+                </ul>
+        HTML;
+} else {
+    $template .= <<<HTML
+        <a class="nav-link p-3" href="/avtoservis/login">
+        Вход
+        </a>
+    HTML;    
 }
+        $template .= "</nav></header>";
+
+        // Добавим flash сообщение
         if (isset($_SESSION['flash'])) {
-            $template .= <<<HTML
+            $template .= <<<END
                 <div id="liveAlertBtn" class="alert alert-info alert-dismissible" role="alert">
                     <div>{$_SESSION['flash']}</div>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
                     onclick="this.parentNode.style.display='none';"></button>
                 </div>
-            HTML;
+            END;
             unset($_SESSION['flash']);
         }
-        
-$template .=  <<<HTML
 
+        $template.= <<<HTML
             %s
-
-            <footer class="mt-5">
+            <footer class="mt-3 p-3">
                 © 2025 «Кемеровский кооперативный техникум»
             <footer>
         </body>
