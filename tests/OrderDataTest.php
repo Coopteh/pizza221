18ad9c85230e9c89e3366153019569dc6016d528
Роман <v.milevskiy@coopteh.ru>
2025-10-06 08:44:26 +0300

    исправлена ошибочка

--- tests/OrderDataTest.php
+++ tests/OrderDataTest.php
@@ -19,7 +19,7 @@ class OrderDataTest extends TestCase
     }
 
     public function testValidateOrderData(): void {
-        $this->assertSame( false, 
+        $this->assertSame( true, 
                            $this->obj->validate($this->data) );
     }
 
